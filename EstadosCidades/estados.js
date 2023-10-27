999524febb276491dab99f6bc53e481a6c9b8ce1
Giovana <23111728@CFP127ALUNO.LOCAL>
2023-10-27 08:47:01 -0300

    Array e JSON

---
+++ EstadosCidades/estados.js
@@ -0,0 +1,17 @@
+var redline = require('readline')
+var estadosCidades = require('./modulo/estados_cidades')
+
+function getListaDeEstados(){
+    const uf = []
+    const quntidade = arrayEstados.length
+    arrayEstados.array.forEach(function(a){
+        uf.push(sigla)
+    });
+        const jsonEstado = {}
+        jsonEstado.uf = uf
+        jsonEstado.quantidade = quantidade
+
+    return jsonEstado
+    console.log(getListaDeEstados);
+}
+
